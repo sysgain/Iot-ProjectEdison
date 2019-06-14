1271c9b5e152b6fe35b6ad0ce3f42373976f304a
Your Name <you@example.com>
2019-06-14 14:40:34 +0530

    updated

--- scripts/deploy1.sh
+++ scripts/deploy1.sh
@@ -12,11 +12,10 @@ sudo apt-get install apt-transport-https lsb-release ca-certificates curl softwa
 sleep 60
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 sudo apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv --keyserver packages.microsoft.com --recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF
-sleep 10s
 sudo apt-get update
-sleep 60
+sleep 10
 sudo apt-get install azure-cli
-sleep 60
+sleep 10
 az -v
 if [ $? -eq 0 ]
 then
@@ -33,9 +32,9 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sleep 60
+sleep 10
 sudo apt-get install docker-ce docker-compose -y
-sleep 60
+sleep 10
 docker -v
 if [ $? -eq 0 ]
 then
